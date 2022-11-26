720a753a024220e25b4057ce98276eebe7346a05
Renato <rena.f.oliveira@gmail.com>
2022-11-26 12:07:09 -0300

    add CategoryResource method GET findById

--- dscatalog/backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ dscatalog/backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -1,8 +1,10 @@
 package com.devsuperior.dscatalog.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.dscatalog.dto.CategoryDTO;
 import com.devsuperior.dscatalog.entities.Category;
@@ -17,9 +19,17 @@ public class CategoryService {
 		this.categoryRepository = categoryRepository;
 	}
 
+	@Transactional(readOnly = true)
 	public List<CategoryDTO> findAll() {
 		List<Category> list = categoryRepository.findAll();
 		return list.stream().map(CategoryDTO::new).toList();
+	}
+
+	@Transactional(readOnly = true)
+	public CategoryDTO findById(Long id) {
+		Optional<Category> obj = categoryRepository.findById(id);
+		Category entity = obj.get();
+		return new CategoryDTO(entity);
 	}
 
 }
