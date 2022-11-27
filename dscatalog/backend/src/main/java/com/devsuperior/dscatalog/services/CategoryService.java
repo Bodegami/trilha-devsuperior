6d54eddada0c4339083dceb3609e3532b22f2c93
Renato <rena.f.oliveira@gmail.com>
2022-11-26 23:44:18 -0300

    CategoryResource method DELETE Delete category

--- dscatalog/backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ dscatalog/backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -5,12 +5,15 @@ import java.util.Optional;
 
 import javax.persistence.EntityNotFoundException;
 
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.dscatalog.dto.CategoryDTO;
 import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.repositories.CategoryRepository;
+import com.devsuperior.dscatalog.services.exceptions.DatabaseException;
 import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
 
 
@@ -55,6 +58,17 @@ public class CategoryService {
 		catch (EntityNotFoundException e) {
 			throw new ResourceNotFoundException(String.format("id %d not found!", id));
 		}
+	}
+
+	public void delete(Long id) {
+		try {
+			categoryRepository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(String.format("id %d not found!", id));
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrity violation!!");
+		}
+		
 	}
 
 }
