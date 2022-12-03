b5255c6fbef4b2c45f1987c7ed03d8d36edbf401
Renato <rena.f.oliveira@gmail.com>
2022-12-03 18:40:44 -0300

    iniciando projeto aula-junit

---
+++ aula-junit/src/tests/entities/AccountTests.java
@@ -0,0 +1,27 @@
+package tests.entities;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import entities.Account;
+
+public class AccountTests {
+	
+	@Test
+	public void depositShouldIncreaseBalanceWhenPositiveAmount() {
+		//Pattern AAA
+		
+		//ARRANGE
+		double amount = 200.0;
+		double expectedValue = 196.0;
+		Account acc = new Account(1L, 0.0);
+		
+		//ACT
+		acc.deposit(amount);
+		
+		
+		//ASSERT
+		Assertions.assertEquals(expectedValue, acc.getBalance());	
+	}
+
+}
