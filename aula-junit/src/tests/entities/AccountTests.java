8c7f478995d6ca821a8431acdb7e1c8ceac1d29a
Renato <rena.f.oliveira@gmail.com>
2022-12-04 11:26:00 -0300

    add fullWithdrawShouldClearBalanceAndReturnFullBalance

--- aula-junit/src/tests/entities/AccountTests.java
+++ aula-junit/src/tests/entities/AccountTests.java
@@ -27,8 +27,6 @@ public class AccountTests {
 	
 	@Test
 	public void depositShouldDoNothingNegativeAmount() {
-
-
 		double expectedValue = 100.0;
 		Account acc = AccountFactory.createAccount(expectedValue);
 		double amount = -200.0;
@@ -36,6 +34,20 @@ public class AccountTests {
 		acc.deposit(amount);
 		
 		Assertions.assertEquals(expectedValue, acc.getBalance());	
+	}
+	
+	@Test
+	public void fullWithdrawShouldClearBalanceAndReturnFullBalance() {
+		
+		double expectedValue = 0.0;
+		double initialBalance = 800.0;
+		Account acc = AccountFactory.createAccount(initialBalance);
+		
+		double result = acc.fullWithdraw();
+		
+		Assertions.assertTrue(expectedValue == acc.getBalance());
+		Assertions.assertTrue(result == initialBalance);
+		
 	}
 
 }
