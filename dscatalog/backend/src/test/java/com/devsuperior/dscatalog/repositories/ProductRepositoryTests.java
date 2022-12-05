ef51dc9ac1ffe13ddd97534b985a67db2ad6b0c9
Renato <rena.f.oliveira@gmail.com>
2022-12-05 00:01:09 -0300

    add ProductRepositoryTests

---
+++ dscatalog/backend/src/test/java/com/devsuperior/dscatalog/repositories/ProductRepositoryTests.java
@@ -0,0 +1,42 @@
+package com.devsuperior.dscatalog.repositories;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.dao.EmptyResultDataAccessException;
+
+import com.devsuperior.dscatalog.entities.Product;
+
+@DataJpaTest
+public class ProductRepositoryTests {
+	
+	@Autowired
+	private ProductRepository repository;
+	
+	@Test
+	public void deleteShouldDeleteObjectWhenIdExists() {
+		
+		long expectedId = 1L;
+		
+		repository.deleteById(expectedId);
+		
+		Optional<Product> result = repository.findById(expectedId);
+		Assertions.assertFalse(result.isPresent());
+	}
+	
+	@Test
+	public void deleteShouldThrowEmptyResultDataAccessExceptionWhenIdDoesNotExists() {
+		
+		long nonExistingId = 1000L;
+		
+		Assertions.assertThrows(EmptyResultDataAccessException.class, () -> {
+			repository.deleteById(nonExistingId);
+		});
+	}
+	
+	
+
+}
